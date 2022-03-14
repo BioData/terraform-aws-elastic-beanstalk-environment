c65e5529d052262ff44c7318dc8fb3e1797c116b
Simon Weil <simon.weil@biodata.com>
2022-03-14 17:01:11 +0200

    Harden ec2 role

--- main.tf
+++ main.tf
@@ -86,13 +86,6 @@ data "aws_iam_policy_document" "ec2" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "elastic_beanstalk_multi_container_docker" {
-  count = local.enabled ? 1 : 0
-
-  role       = join("", aws_iam_role.ec2.*.name)
-  policy_arn = "arn:${local.partition}:iam::aws:policy/AWSElasticBeanstalkMulticontainerDocker"
-}
-
 resource "aws_iam_role" "ec2" {
   count = local.enabled ? 1 : 0
 
@@ -110,14 +103,14 @@ resource "aws_iam_role_policy" "default" {
 }
 
 resource "aws_iam_role_policy_attachment" "web_tier" {
-  count = local.enabled ? 1 : 0
+  count = local.enabled && var.tier == "WebServer" ? 1 : 0
 
   role       = join("", aws_iam_role.ec2.*.name)
   policy_arn = "arn:${local.partition}:iam::aws:policy/AWSElasticBeanstalkWebTier"
 }
 
 resource "aws_iam_role_policy_attachment" "worker_tier" {
-  count = local.enabled ? 1 : 0
+  count = local.enabled && var.tier == "Worker" ? 1 : 0
 
   role       = join("", aws_iam_role.ec2.*.name)
   policy_arn = "arn:${local.partition}:iam::aws:policy/AWSElasticBeanstalkWorkerTier"
@@ -163,175 +156,13 @@ resource "aws_ssm_activation" "ec2" {
   tags               = var.tags
 }
 
-data "aws_iam_policy_document" "default" {
-  count = local.enabled ? 1 : 0
-
-  statement {
-    actions = [
-      "elasticloadbalancing:DescribeInstanceHealth",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticloadbalancing:DescribeTargetHealth",
-      "ec2:DescribeInstances",
-      "ec2:DescribeInstanceStatus",
-      "ec2:GetConsoleOutput",
-      "ec2:AssociateAddress",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeSecurityGroups",
-      "sqs:GetQueueAttributes",
-      "sqs:GetQueueUrl",
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeScalingActivities",
-      "autoscaling:DescribeNotificationConfigurations",
-    ]
-
-    resources = ["*"]
-
-    effect = "Allow"
-  }
-
-  statement {
-    sid = "AllowOperations"
-
-    actions = [
-      "autoscaling:AttachInstances",
-      "autoscaling:CreateAutoScalingGroup",
-      "autoscaling:CreateLaunchConfiguration",
-      "autoscaling:DeleteLaunchConfiguration",
-      "autoscaling:DeleteAutoScalingGroup",
-      "autoscaling:DeleteScheduledAction",
-      "autoscaling:DescribeAccountLimits",
-      "autoscaling:DescribeAutoScalingGroups",
-      "autoscaling:DescribeAutoScalingInstances",
-      "autoscaling:DescribeLaunchConfigurations",
-      "autoscaling:DescribeLoadBalancers",
-      "autoscaling:DescribeNotificationConfigurations",
-      "autoscaling:DescribeScalingActivities",
-      "autoscaling:DescribeScheduledActions",
-      "autoscaling:DetachInstances",
-      "autoscaling:PutScheduledUpdateGroupAction",
-      "autoscaling:ResumeProcesses",
-      "autoscaling:SetDesiredCapacity",
-      "autoscaling:SetInstanceProtection",
-      "autoscaling:SuspendProcesses",
-      "autoscaling:TerminateInstanceInAutoScalingGroup",
-      "autoscaling:UpdateAutoScalingGroup",
-      "cloudwatch:PutMetricAlarm",
-      "ec2:AssociateAddress",
-      "ec2:AllocateAddress",
-      "ec2:AuthorizeSecurityGroupEgress",
-      "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:CreateSecurityGroup",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DescribeAccountAttributes",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeImages",
-      "ec2:DescribeInstances",
-      "ec2:DescribeKeyPairs",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSnapshots",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeVpcs",
-      "ec2:DisassociateAddress",
-      "ec2:ReleaseAddress",
-      "ec2:RevokeSecurityGroupEgress",
-      "ec2:RevokeSecurityGroupIngress",
-      "ec2:TerminateInstances",
-      "ecs:CreateCluster",
-      "ecs:DeleteCluster",
-      "ecs:DescribeClusters",
-      "ecs:RegisterTaskDefinition",
-      "elasticbeanstalk:*",
-      "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
-      "elasticloadbalancing:ConfigureHealthCheck",
-      "elasticloadbalancing:CreateLoadBalancer",
-      "elasticloadbalancing:DeleteLoadBalancer",
-      "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-      "elasticloadbalancing:DescribeInstanceHealth",
-      "elasticloadbalancing:DescribeLoadBalancers",
-      "elasticloadbalancing:DescribeTargetHealth",
-      "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-      "elasticloadbalancing:DescribeTargetGroups",
-      "elasticloadbalancing:RegisterTargets",
-      "elasticloadbalancing:DeregisterTargets",
-      "iam:ListRoles",
-      "iam:PassRole",
-      "logs:CreateLogGroup",
-      "logs:PutRetentionPolicy",
-      "rds:DescribeDBEngineVersions",
-      "rds:DescribeDBInstances",
-      "rds:DescribeOrderableDBInstanceOptions",
-      "s3:GetObject",
-      "s3:GetObjectAcl",
-      "s3:ListBucket",
-      "sns:CreateTopic",
-      "sns:GetTopicAttributes",
-      "sns:ListSubscriptionsByTopic",
-      "sns:Subscribe",
-      "sqs:GetQueueAttributes",
-      "sqs:GetQueueUrl",
-      "codebuild:CreateProject",
-      "codebuild:DeleteProject",
-      "codebuild:BatchGetBuilds",
-      "codebuild:StartBuild",
-    ]
-
-    resources = ["*"]
-
-    effect = "Allow"
-  }
-
-  statement {
-    sid = "AllowS3OperationsOnElasticBeanstalkBuckets"
-
-    actions = [
-      "s3:*"
-    ]
-
-    resources = [
-      #bridgecrew:skip=BC_AWS_IAM_57:Skipping "Ensure IAM policies does not allow write access without constraint"
-      #bridgecrew:skip=BC_AWS_IAM_56:Skipping "Ensure IAM policies do not allow permissions management / resource exposure without constraint"
-      #bridgecrew:skip=BC_AWS_IAM_55:Skipping "Ensure IAM policies do not allow data exfiltration"
-      "arn:${local.partition}:s3:::*"
-    ]
-
-    effect = "Allow"
-  }
-
-  statement {
-    sid = "AllowDeleteCloudwatchLogGroups"
-
-    actions = [
-      "logs:DeleteLogGroup"
-    ]
-
-    resources = [
-      "arn:${local.partition}:logs:*:*:log-group:/aws/elasticbeanstalk*"
-    ]
-
-    effect = "Allow"
-  }
-
-  statement {
-    sid = "AllowCloudformationOperationsOnElasticBeanstalkStacks"
-
-    actions = [
-      "cloudformation:*"
-    ]
-
-    resources = [
-      "arn:${local.partition}:cloudformation:*:*:stack/awseb-*",
-      "arn:${local.partition}:cloudformation:*:*:stack/eb-*"
-    ]
-
-    effect = "Allow"
-  }
+data "aws_iam_policy_document" "empty" {
 }
 
 data "aws_iam_policy_document" "extended" {
   count = local.enabled ? 1 : 0
 
-  source_json   = join("", data.aws_iam_policy_document.default.*.json)
+  source_json   = join("", data.aws_iam_policy_document.empty.*.json)
   override_json = var.extended_ec2_policy_document
 }
 
@@ -366,7 +197,6 @@ locals {
       name      = "ManagedSecurityGroup"
       value     = var.loadbalancer_managed_security_group
     },
-
     {
       namespace = "aws:elb:listener"
       name      = "ListenerProtocol"
